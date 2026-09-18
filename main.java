9124f67f4de80f9e79542714e599d2157ceaef04
Janberk-San <janberksan@vt.edu>
2026-09-18 13:58:40 -0400

    TEST

--- main.java
+++ main.java
@@ -1,3 +1,8 @@
 public class main {
-    
+
+    // The JVM looks for this exact method signature to start the program
+    public static void main(String[] args) {
+        // Your application logic starts here
+        System.out.println("Hello, World!");
+    }
 }
